5be80a7f78d3c9c00e6144a2850e9220852c2edf
Daniel Micay <danielmicay@gmail.com>
2018-11-22 15:25:54 -0500

    set proper values for PRODUCT_MODEL

--- aosp_crosshatch.mk
+++ aosp_crosshatch.mk
@@ -65,4 +65,4 @@ PRODUCT_MANUFACTURER := Google
 PRODUCT_NAME := aosp_crosshatch
 PRODUCT_DEVICE := crosshatch
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on crosshatch
+PRODUCT_MODEL := Pixel 3 XL
