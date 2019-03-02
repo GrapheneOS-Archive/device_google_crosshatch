db3f12f691738df0ab9313d682183b70609a3a92
Daniel Micay <danielmicay@gmail.com>
2019-03-01 23:22:33 -0500

    disable system_other preloads_copy [1/2]

--- device.mk
+++ device.mk
@@ -144,10 +144,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     bootctrl.sdm845
 
-# Script that copies preloads directory from system_other to data partition
-PRODUCT_COPY_FILES += \
-    device/google/crosshatch/preloads_copy.sh:system/bin/preloads_copy.sh
-
 AB_OTA_UPDATER := true
 
 AB_OTA_PARTITIONS += \
