76939acfe6f5ce1e7ae8caf570b5b7d9b64b9b66
Daniel Micay <danielmicay@gmail.com>
2021-07-15 20:25:42 -0400

    remove aosp_ prefix from product names

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,11 +15,11 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_crosshatch.mk \
-    $(LOCAL_DIR)/aosp_blueline.mk \
+    $(LOCAL_DIR)/crosshatch.mk \
+    $(LOCAL_DIR)/blueline.mk \
     $(LOCAL_DIR)/aosp_crosshatch_hwasan.mk \
     $(LOCAL_DIR)/aosp_blueline_hwasan.mk \
 
 COMMON_LUNCH_CHOICES := \
-    aosp_crosshatch-userdebug \
-    aosp_blueline-userdebug
+    crosshatch-userdebug \
+    blueline-userdebug
