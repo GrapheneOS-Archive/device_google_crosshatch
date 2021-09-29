df5b66035c6ffb42c4a243815aa6610a3a7b8cd7
flawedworld <flawedworld@flawed.world>
2021-09-29 04:43:42 +0100

    Use APNs and CarrierConfig from APV

--- blueline.mk
+++ blueline.mk
@@ -3,3 +3,5 @@ $(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
 
 # Remove AOSP prefix from product name
 PRODUCT_NAME := blueline
+# Tell build system not to bundle sample APNs from AOSP
+OEM_APNS := true
