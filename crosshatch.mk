c3390d8cee94f3365a45113697943a7277023f5e
flawedworld <flawedworld@flawed.world>
2021-09-29 04:43:42 +0100

    Use APNs and CarrierConfig from APV

--- crosshatch.mk
+++ crosshatch.mk
@@ -3,3 +3,5 @@ $(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
 
 # Remove AOSP prefix from product name
 PRODUCT_NAME := crosshatch
+# Tell build system not to bundle sample APNs from AOSP
+OEM_APNS := true
