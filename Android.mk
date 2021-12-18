894fcfaccf3e07893ae1371e2559e49760b48a00
flawedworld <flawedworld@flawed.world>
2021-12-18 01:41:01 +0000

    Add blank apdp.img and msadp.img

--- Android.mk
+++ Android.mk
@@ -19,4 +19,5 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(USES_DEVICE_GOOGLE_B1C1),true)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+  include device/google/crosshatch/AndroidBoardCommon.mk
 endif
